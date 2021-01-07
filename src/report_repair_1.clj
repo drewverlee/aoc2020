cf83c47e15037baa3eeda25999f93dbdc36e0231
drewverlee <drew.verlee@gmail.com>
2021-01-07 09:56:45 -0600

    add comment to day one part 1

--- src/report_repair_1.clj
+++ src/report_repair_1.clj
@@ -2,6 +2,9 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
+
+
+;; part 1
 ;; find the two entries that sum to 2020 and then multiply those two numbers together.
 (let [n (->> "one" io/resource slurp str/split-lines (map #(Integer/parseInt %)))]
   (first
