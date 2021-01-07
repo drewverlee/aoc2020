7cfc46c7a73612c92f12d8a5155bc5bd3a90f560
drewverlee <drew.verlee@gmail.com>
2021-01-07 09:57:14 -0600

    solution day1-part2

--- src/report_repair_1.clj
+++ src/report_repair_1.clj
@@ -14,3 +14,21 @@
             :when (= 2020 (+ b a))]
         (* a b)))))
 ;; => 145875
+
+;; --- Part Two ---
+
+;; find three numbers in your expense report that meet the same criteria.
+
+;; Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+
+;; In your expense report, what is the product of the three entries that sum to 2020?
+
+(let [n (->> "one" io/resource slurp str/split-lines (map #(Integer/parseInt %)))]
+  (first
+    (set
+      (for [a     n
+            b     n
+            c     n
+            :when (= 2020 (+ b a c))]
+        (* a b c)))))
+;; => 69596112
