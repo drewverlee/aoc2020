3fd720b742cd29d19c205f6a44a5dda587f4df33
drewverlee <drew.verlee@gmail.com>
2021-01-10 15:51:27 -0600

    add solution to day 5 part 2

--- src/binary_boarding_5.clj
+++ src/binary_boarding_5.clj
@@ -31,3 +31,31 @@
   last
   :seat-ID)
 ;; => 878
+
+;; --- Part Two ---
+
+(->> "input-5"
+  io/resource
+  slurp
+  str/split-lines
+  (map #(str/split % #""))
+  (map (fn [i]
+         (loop [[rs re] [0 127]
+                [cs ce] [0 7]
+                d       i]
+           (if (empty? d)
+             (+ cs (* rs 8))
+             (let [f #(+ %1 (int (%3 (/ (- %2 %1) 2))))]
+               (recur
+                 (case (first d)
+                   "F" [rs (f rs re floor)]
+                   "B" [(f rs re ceil) re]
+                   [rs re])
+                 (case (first d)
+                   "L" [cs (f cs ce floor)]
+                   "R" [(f cs ce ceil) ce]
+                   [cs ce])
+                 (rest d)))))))
+  sort
+  (reduce  #(if (= (inc %1) %2) %2 (reduced (inc %1)))))
+;; => 504
