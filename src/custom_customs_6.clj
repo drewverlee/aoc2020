586659411f23b380ecfe815d39af4462bb6d5db4
drewverlee <drew.verlee@gmail.com>
2021-01-10 16:36:33 -0600

    add day 6 part 2 solution

--- src/custom_customs_6.clj
+++ src/custom_customs_6.clj
@@ -4,6 +4,8 @@
 
 ;; --- Day 6: Custom Customs ---
 
+;; ---- part 1 --------
+
 (->> "input-6"
   io/resource
   slurp
@@ -14,3 +16,18 @@
   (map count)
   (apply +))
 ;; => 6680
+
+;; ---------- part 2 ----------------------
+
+(->> "input-6"
+  io/resource
+  slurp
+  str/split-lines
+  (partition-by #(str/blank? %))
+  (remove #(= [""] %))
+  (map #(hash-map :a (str/join "" %) :g (count %)))
+  (map #(assoc % :d (-> % :a frequencies)))
+  (map (fn [{:keys [d g] :as m}] (assoc m :yes (->> g ((group-by val d)) count))))
+  (map :yes)
+  (apply +))
+;; => 3117
